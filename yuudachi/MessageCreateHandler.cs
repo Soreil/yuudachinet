ef970cc428d4b84dcd39579cbd85827eda5f03ff
sjon <sjon@outlook.com>
2025-06-14 17:33:36 +0200

    Proof of concept chaining, we should be cloning the conversation when we fork

--- yuudachi/MessageCreateHandler.cs
+++ yuudachi/MessageCreateHandler.cs
@@ -4,24 +4,28 @@ using NetCord.Gateway;
 using NetCord.Hosting.Gateway;
 using NetCord.Rest;
 
+using yuudachi.Groq;
+
 namespace yuudachi;
 
 [GatewayEvent(nameof(GatewayClient.MessageCreate))]
-public class MessageCreateHandler(ILogger<MessageCreateHandler> logger, Groq.GroqConversationHistory convoHistory) : IGatewayEventHandler<Message>
+public class MessageCreateHandler(ILogger<MessageCreateHandler> logger, Groq.GroqConversationHistory convoHistory, GroqClient groqClient) : IGatewayEventHandler<NetCord.Gateway.Message>
 {
-    public async ValueTask HandleAsync(Message message)
+    public async ValueTask HandleAsync(NetCord.Gateway.Message message)
     {
         logger.LogInformation("{}", message.Content);
 
-        if (message.ReferencedMessage is not null && convoHistory.Conversations.ContainsKey(message.ReferencedMessage.Id))
+        if (message.ReferencedMessage is not null && convoHistory.Conversations.TryGetValue(message.ReferencedMessage.Id, out Conversation? convo))
         {
-            var oldConvo = convoHistory.Conversations[message.ReferencedMessage.Id];
+            convo.AddMessage(message.Content);
+            var res = await groqClient.ConversationResult(convo);
 
-            var body = "Cool reply king!";
+
+            var body = res.Choices[0].Message.Content;
 
             var resp = await message.ReplyAsync(new ReplyMessageProperties() { Content = body });
 
-            convoHistory.Conversations.Add(resp.Id, oldConvo);
+            convoHistory.Conversations.Add(resp.Id, convo);
         }
 
         return;
