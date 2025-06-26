aab5b684b8ae78564a52447387b9d88e748050ae
Sjon Kosse <sjon@outlook.com>
2025-06-27 00:09:27 +0200

    Initial formatting work

--- yuudachi/Groq/GroqConversationHistory.cs
+++ yuudachi/Groq/GroqConversationHistory.cs
@@ -2,5 +2,46 @@
 
 public class GroqConversationHistory
 {
-    public Dictionary<ulong, Conversation> Conversations { get; } = [];
+    public Dictionary<ulong, ConversationWithFormattingOptions> Conversations { get; } = [];
+}
+
+public class ConversationWithFormattingOptions
+{
+    public required Conversation Conversation { get; init; }
+    public required IFormattingOptions FormattingOptions { get; init; }
+}
+
+public interface IFormattingOptions
+{
+    public IEnumerable<string> Format(string content);
+}
+
+public class TruncatingFormatter : IFormattingOptions
+{
+    private readonly int maxLength;
+    public TruncatingFormatter(int maxLength)
+    {
+        this.maxLength = maxLength;
+    }
+    public IEnumerable<string> Format(string content)
+    {
+        if (content.Length > maxLength)
+        {
+            return [string.Concat(content.AsSpan(0, maxLength), "... (truncated)")];
+        }
+        return [content];
+    }
+}
+
+public class NaiveChunkingFormatter : IFormattingOptions
+{
+    private readonly int chunkSize;
+    public NaiveChunkingFormatter(int chunkSize)
+    {
+        this.chunkSize = chunkSize;
+    }
+    public IEnumerable<string> Format(string content)
+    {
+        return (IEnumerable<string>)content.Chunk(chunkSize);
+    }
 }
